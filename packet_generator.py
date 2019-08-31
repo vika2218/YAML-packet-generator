19f307e5dcae18a0cbb3099453ee121e51063e92
VINIT KANVINDE <52331868+vika2218@users.noreply.github.com>
2019-08-31 14:10:18 -0600

    Create packet_generator.py

---
+++ packet_generator.py
@@ -0,0 +1,29 @@
+from scapy.all import *
+import oyaml as yaml
+
+f= (open('packet_generator.yaml', 'r')).read()
+dict1= yaml.load(f , Loader=yaml.FullLoader)
+
+
+def geneatingPacket():
+  h= []
+  for p in dict1:
+    for i in dict1[p]:
+        if i != "Attributes":
+            for j in dict1[p][i]:
+                exec(j)
+
+    for i in dict1[p]:
+        if not i == 'Packet' and i != 'Attributes':
+            hdr = eval(i)()			# Creating raw headers ex. hdr= IP() before giving the attributes
+            for j in dict1[p][i]:		# This will add attributes to already created raw headers
+                    k='hdr.'+j		# ex. hdr.src='1.1.1.1'
+                    exec(k)
+            h.append(hdr)
+            #print(h)
+  pkt=h[0]
+  for j in range(1,len(h)):
+    pkt= pkt/h[j]
+  pkt.show()
+
+geneatingPacket()
